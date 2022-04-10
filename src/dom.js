7017a78307f86470621db8551e82a9fe2e1d04ae
Elvinas <elviz.nejuz@gmail.com>
2022-04-10 13:26:55 +0300

    added dropdown menu

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,18 @@
 const domController = (() => {
   const toggleDropDown = () => {
+    document.querySelector(".arrow-up").classList.toggle("rotate");
     document.querySelector(".nav__dropdown").classList.toggle("show");
   };
+  document.querySelector("nav").addEventListener("click", (e) => {
+    if (e.target.nodeName !== "NAV") {
+      let target = e.target;
+      console.log(target);
+      while (target.nodeName !== "BUTTON") {
+        console.log(target.parrentElement);
+        target = target.parrentElement;
+      }
+    }
+  });
 
   document.querySelector(".nav__projects").onclick = toggleDropDown;
   return { toggleDropDown };

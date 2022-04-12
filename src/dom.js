798e02696a2f8212da1db98aa19b4658576e9e03
Elvinas <elviz.nejuz@gmail.com>
2022-04-12 20:28:04 +0300

    added tasks to the projects

--- src/dom.js
+++ src/dom.js
@@ -3,6 +3,7 @@ import projectImg from "./images/singleproj.svg";
 import editIcon from "./images/edit.svg";
 import deleteIcon from "./images/delete.svg";
 import { projectFunctions } from "./projectFunctions";
+import { Task } from "./task";
 
 const domController = (() => {
   const dropDownContainer = document.querySelector(".nav__dropdown");
@@ -13,11 +14,11 @@ const domController = (() => {
     projectsList = [Project("Project 1", "This is example of a project.")];
   }
   localStorage.setItem("projects", JSON.stringify(projectsList));
-
   const open = document.getElementById("add_project");
   const modal_container = document.getElementById("modal-container");
   let main = document.querySelector("main");
 
+  /// TODO: Update this method
   const updateProjects = () => {
     dropDownContainer.innerHTML = "";
     let i = 0;
@@ -181,7 +182,6 @@ const domController = (() => {
     edit.classList.add("modal__button");
     form.appendChild(edit);
     form.addEventListener("submit", () => {
-      console.log(inputName.value);
       const edittedProject = Project(inputName.value, inputDescription.value);
       projectsList[projectsList.indexOf(project)] = edittedProject;
       localStorage.setItem("projects", JSON.stringify(projectsList));
@@ -221,7 +221,6 @@ const domController = (() => {
 
   const createProjectMain = (project) => {
     const main = document.createElement("main");
-
     const headerDiv = document.createElement("div");
     headerDiv.classList.add("main__header-div");
 
@@ -253,7 +252,171 @@ const domController = (() => {
     projectDescription.textContent = project.description;
     main.appendChild(projectDescription);
 
+    const addTask = document.createElement("button");
+    addTask.textContent = "Add task";
+
+    addTask.addEventListener("click", () => {
+      modal_container.classList.add("show-modal");
+      modal_container.appendChild(addTaskModal());
+    });
+    main.appendChild(addTask);
+
+    project.listOfTasks.forEach((task) => {
+      main.appendChild(createTaskDiv(task));
+    });
+
     return main;
+  };
+
+  const createTaskDiv = (task) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("project__task");
+
+    const taskPara = document.createElement("p");
+    taskPara.textContent = task.task;
+    wrapper.appendChild(taskPara);
+
+    const priority = document.createElement("p");
+    priority.textContent = task.priority;
+    wrapper.appendChild(priority);
+
+    const dueDate = document.createElement("p");
+    dueDate.textContent = task.dueDate;
+    wrapper.appendChild(dueDate);
+    return wrapper;
+  };
+
+  const addTaskModal = () => {
+    const project = projectFunctions.getProjectByName(
+      projectsList,
+      document.querySelector(".main__header").textContent
+    );
+
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    const header = document.createElement("p");
+    header.textContent = "Add task";
+    header.classList.add("modal__header");
+    modal.appendChild(header);
+
+    const form = document.createElement("form");
+    const divTask = document.createElement("div");
+
+    const labelForTask = document.createElement("label");
+    labelForTask.setAttribute("for", "task");
+    labelForTask.textContent = "Task Title: ";
+    divTask.appendChild(labelForTask);
+
+    const inputTask = document.createElement("input");
+    inputTask.type = "text";
+    inputTask.id = "task";
+    inputTask.required = true;
+    inputTask.maxLength = 20;
+    divTask.append(inputTask);
+    form.appendChild(divTask);
+
+    const divDescription = document.createElement("div");
+
+    const labelForTaskDesc = document.createElement("label");
+    labelForTaskDesc.setAttribute("for", "description");
+    labelForTaskDesc.textContent = "Task Description: ";
+    divDescription.appendChild(labelForTaskDesc);
+
+    const inputDescription = document.createElement("textarea");
+    inputDescription.id = "description";
+    inputDescription.required = true;
+    inputDescription.rows = 3;
+    inputDescription.cols = 20;
+    divDescription.appendChild(inputDescription);
+    form.appendChild(divDescription);
+
+    const divPriority = document.createElement("div");
+
+    const labelForPriority = document.createElement("label");
+    labelForPriority.setAttribute("for", "priority");
+    labelForPriority.textContent = "Task Priority: ";
+    divPriority.appendChild(labelForPriority);
+
+    const inputPriority = document.createElement("select");
+    inputPriority.id = "priority";
+
+    const option1 = document.createElement("option");
+    option1.value = "low";
+    option1.textContent = "Low";
+    inputPriority.appendChild(option1);
+
+    const option2 = document.createElement("option");
+    option2.value = "medium";
+    option2.textContent = "Medium";
+    inputPriority.appendChild(option2);
+
+    const option3 = document.createElement("option");
+    option3.value = "high";
+    option3.textContent = "High";
+    inputPriority.appendChild(option3);
+
+    const option4 = document.createElement("option");
+    option4.value = "urgent";
+    option4.textContent = "Urgent";
+    inputPriority.appendChild(option4);
+    divPriority.appendChild(inputPriority);
+
+    form.appendChild(divPriority);
+
+    const divDueDate = document.createElement("div");
+
+    const labelForDueDate = document.createElement("label");
+    labelForDueDate.setAttribute("for", "dueDate");
+    labelForDueDate.textContent = "Due Date: ";
+    divDueDate.appendChild(labelForDueDate);
+
+    const inputDueDate = document.createElement("input");
+    // TODO: Extract this function
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    today = yyyy + "-" + mm + "-" + dd;
+    inputDueDate.type = "date";
+    inputDueDate.id = "dueDate";
+    inputDueDate.min = today;
+    inputDueDate.required = true;
+    divDueDate.appendChild(inputDueDate);
+    form.appendChild(divDueDate);
+
+    const add = document.createElement("button");
+    add.textContent = "ADD";
+    add.classList.add("modal__button");
+    form.appendChild(add);
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const newTask = Task(
+        inputTask.value,
+        inputDescription.value,
+        inputPriority.value,
+        inputDueDate.value
+      );
+      projectsList[projectsList.indexOf(project)].listOfTasks.push(newTask);
+      localStorage.setItem("projects", JSON.stringify(projectsList));
+      modal_container.classList.remove("show-modal");
+      modal_container.innerHTML = "";
+      document.body.removeChild(main);
+      main = createProjectMain(projectsList[projectsList.indexOf(project)]);
+      document.body.appendChild(main);
+    });
+    modal.appendChild(form);
+
+    return modal;
   };
   open.addEventListener("click", () => {
     modal_container.appendChild(addProjectModal());
---
+++ src/task.js
@@ -0,0 +1,4 @@
+const Task = (task, description, priority, dueDate) => {
+  return { task, description, priority, dueDate };
+};
+export { Task };
